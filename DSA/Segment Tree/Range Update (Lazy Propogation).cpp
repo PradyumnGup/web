b0513f4d3ae6ee4b116fc8b881cee0912ec0a7ef
Pradyumn Gupta <102296143+PradyumnGup@users.noreply.github.com>
2025-01-29 22:08:05 +0530

    Update Range Update (Lazy Propogation).cpp

--- DSA/Segment Tree/Range Update (Lazy Propogation).cpp
+++ DSA/Segment Tree/Range Update (Lazy Propogation).cpp
@@ -11,29 +11,28 @@
 //T.C : O(logn)
 //S.C : O(n)
 //Assuming we have defined segmentTree[4*n] and lazyTree[4*n]
-void updateRange(int start, int end, int i, int l, int r, int val) {
+void applyLazy(int i, int l, int r) {
     if (lazyTree[i] != 0) {
-        segmentTree[i] += (r - l + 1) * lazyTree[i];
-      
-        if (l != r) { //Not a leaf node
-            lazyTree[i * 2 + 1] += lazyTree[i]; //Left Child in Lazy Tree
-            lazyTree[i * 2 + 2] += lazyTree[i]; //Right Child in Lazy tree
-        }
-        lazyTree[i] = 0;
-    }
+        segmentTree[i] += (long long)(r - l + 1) * lazyTree[i];
 
+        if (l != r) { // If not a leaf node, propagate lazily to children
+            lazyTree[2 * i + 1] += lazyTree[i];
+            lazyTree[2 * i + 2] += lazyTree[i];
+        }
+        lazyTree[i] = 0; // Clear the lazy value after applying
+    }
+}
+void updateRange(int start, int end, int i, int l, int r, int val) {
+    applyLazy(i, l, r);
     //Invalid or out of range
     if (l > end || r < start || l > r)
         return;
 
     //[start...end[ is Exactly withing range of current nod [l..r]
     if (l >= start && r <= end) {
-        segmentTree[i] += (r - l + 1) * val;
-        if (l != r) {
-            lazyTree[i * 2 + 1] += val;
-            lazyTree[i * 2 + 2] += val;
-        }
-        return;
+            lazyTree[i] += val;
+            applyLazy(i, l, r);
+            return;
     }
 
     //Call for left and right subtree
