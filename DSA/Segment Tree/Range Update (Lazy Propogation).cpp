c1074b01f6d472a173601c4b7ac4bd97e85d0994
Pradyumn Gupta <102296143+PradyumnGup@users.noreply.github.com>
2025-01-29 21:28:26 +0530

    Update Range Update (Lazy Propogation).cpp

--- DSA/Segment Tree/Range Update (Lazy Propogation).cpp
+++ DSA/Segment Tree/Range Update (Lazy Propogation).cpp
@@ -13,7 +13,7 @@
 //Assuming we have defined segmentTree[4*n] and lazyTree[4*n]
 void updateRange(int start, int end, int i, int l, int r, int val) {
     if (lazyTree[i] != 0) {
-        segTree[i] += (r - l + 1) * lazyTree[i];
+        segmentTree[i] += (r - l + 1) * lazyTree[i];
       
         if (l != r) { //Not a leaf node
             lazyTree[i * 2 + 1] += lazyTree[i]; //Left Child in Lazy Tree
@@ -28,7 +28,7 @@ void updateRange(int start, int end, int i, int l, int r, int val) {
 
     //[start...end[ is Exactly withing range of current nod [l..r]
     if (l >= start && r <= end) {
-        segTree[i] += (r - l + 1) * val;
+        segmentTree[i] += (r - l + 1) * val;
         if (l != r) {
             lazyTree[i * 2 + 1] += val;
             lazyTree[i * 2 + 2] += val;
@@ -40,5 +40,5 @@ void updateRange(int start, int end, int i, int l, int r, int val) {
     int mid = (l + r) / 2;
     updateRange(start, end, 2*i+1, l, mid, val);
     updateRange(start, end, 2*i+2, mid+1, r, val);
-    segTree[i] = segTree[i * 2 + 1] + segTree[i * 2 + 2];
+    segmentTree[i] = segmentTree[i * 2 + 1] + segmentTree[i * 2 + 2];
 }
