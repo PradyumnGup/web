ffe25c3ea5f52609ecee1320136a68d81b4812ca
Pradyumn Gupta <102296143+PradyumnGup@users.noreply.github.com>
2025-01-29 14:43:40 +0530

    Create Fenwick Tree.cpp

---
+++ DSA/Fenwick Tree.cpp
@@ -0,0 +1,23 @@
+class FenwickTree{
+    public:
+    vector<int>bit;
+    int n;
+    FenwickTree(int n){
+        this->n=n;
+        bit= vector<int>(n+1,0);
+    }
+    void add(int id,int val){
+        while(id<=n){
+            bit[id]=max(bit[id],val);
+            id+=(id&-id);
+        }
+    }
+    int query(int id){
+        int maxAns=0;
+        while(id>0){
+            maxAns=max(maxAns,bit[id]);
+            id-=(id&-id);
+        }
+        return maxAns;
+    }
+};
