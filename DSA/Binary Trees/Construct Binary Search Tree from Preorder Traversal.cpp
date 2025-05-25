60e40228b87ae140f6e23d29962e47fdbf9fdbab
Pradyumn Gupta <102296143+PradyumnGup@users.noreply.github.com>
2025-05-25 17:37:52 +0530

    Update Construct Binary Search Tree from Preorder Traversal.cpp

--- DSA/Binary Trees/Construct Binary Search Tree from Preorder Traversal.cpp
+++ DSA/Binary Trees/Construct Binary Search Tree from Preorder Traversal.cpp
@@ -9,6 +9,40 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//preorder->root,left,right
+//inorder ->left,root,right sorted in bst
+//3,1,4,5,7
+//1,3,4,5,7
+
+//            3
+//         1    4
+//                 5
+//                   7
+//O(n^2) approach
+class Solution {
+public:
+    TreeNode* bst(int preStart,int preEnd,vector<int>&preorder){
+        if(preStart>preEnd)return NULL;
+        int index=preStart;
+        while(index<=preEnd){
+            if(preorder[index]>preorder[preStart]){
+                break;
+            }
+            index++;
+        }
+        
+        TreeNode* root=new TreeNode(preorder[preStart]);
+        root->left=bst(preStart+1,index-1,preorder);
+        root->right=bst(index==preStart?index+1:index,preEnd,preorder);
+        return root;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int n=preorder.size();
+        return bst(0,n-1,preorder);
+    }
+};
+
+//this is o(n).
 class Solution {
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
